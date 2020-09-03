8768a7316c908b435740f9c950e079225688f5c7
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-09-02 23:29:21 -0400

    chore: fix typo

---
+++ build/precache-manifest.ee34759c7240bafa0ceb19725a8088f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f50f10a61a4ce80eb3d0",
+    "url": "/reactnd-project-myreads-starter-1/static/css/main.45a7d569.chunk.css"
+  },
+  {
+    "revision": "f50f10a61a4ce80eb3d0",
+    "url": "/reactnd-project-myreads-starter-1/static/js/main.f50f10a6.chunk.js"
+  },
+  {
+    "revision": "31ea858006fd6fdb6445",
+    "url": "/reactnd-project-myreads-starter-1/static/js/1.31ea8580.chunk.js"
+  },
+  {
+    "revision": "1c06abd462a071dde69b",
+    "url": "/reactnd-project-myreads-starter-1/static/js/runtime~main.1c06abd4.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads-starter-1/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads-starter-1/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads-starter-1/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "7336f3a112f35ffd7002fb86f034ffe2",
+    "url": "/reactnd-project-myreads-starter-1/index.html"
+  }
+];
