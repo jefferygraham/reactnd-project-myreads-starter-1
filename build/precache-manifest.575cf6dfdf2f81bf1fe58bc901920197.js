17cd0b96e0b8f70923506c052e94708fa14e0323
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-09-09 17:05:44 -0400

    change homepage

---
+++ build/precache-manifest.575cf6dfdf2f81bf1fe58bc901920197.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "989eb7791ae568c4edd5",
+    "url": "/reactnd-project-myreads-starter-1/static/css/main.3ee971ad.chunk.css"
+  },
+  {
+    "revision": "989eb7791ae568c4edd5",
+    "url": "/reactnd-project-myreads-starter-1/static/js/main.989eb779.chunk.js"
+  },
+  {
+    "revision": "b7c172ab545a3c88759d",
+    "url": "/reactnd-project-myreads-starter-1/static/js/1.b7c172ab.chunk.js"
+  },
+  {
+    "revision": "1c06abd462a071dde69b",
+    "url": "/reactnd-project-myreads-starter-1/static/js/runtime~main.1c06abd4.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads-starter-1/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads-starter-1/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads-starter-1/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "6e204110d0069eed98c86bab7aa6aca8",
+    "url": "/reactnd-project-myreads-starter-1/index.html"
+  }
+];
